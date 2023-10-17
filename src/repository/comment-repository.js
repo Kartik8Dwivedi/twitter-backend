5a43782637e6bcdc983ce3cbde6236efd0bad63d
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-10-18 02:08:35 +0530

    added comment-service

---
+++ src/repository/comment-repository.js
@@ -0,0 +1,10 @@
+import CrudRepository from "./crud-repository";
+import Comment from "../models/comment";
+
+class CommentRepository extends CrudRepository {
+  constructor() {
+    super(Comment);
+  }
+}
+
+export default CommentRepository;
