99b28ee4a070eb826332bfcf300847447010934a
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-10-18 17:42:01 +0530

    auth services in work

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -8,12 +8,41 @@ class UserService {
   async signup(data) {
     try {
       const user = await this.userRepository.create(data);
-      console.log("user object is", user)
+      console.log("user object is", user);
       user.password = "SAVED_TO_DB";
       return user;
     } catch (error) {
-        console.log("Error in Service layer ", error);
-        return new Error(error)
+      console.log("Error in Service layer ", error);
+      return new Error(error);
+    }
+  }
+
+  async getUserByEmail(email) {
+    try {
+      const user = await this.userRepository.findBy({ email });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signin(data) {
+    try {
+      const user = await this.getUserByEmail(data.email);
+      if (!user) {
+        throw {
+          message: "no user found",
+        };
+      }
+      if (!user.comparePassword(data.password)) {
+        throw {
+          message: "incorrect password",
+        };
+      }
+      const token = user.generateJWTtoken();
+      return token;
+    } catch (error) {
+      throw error;
     }
   }
 }
